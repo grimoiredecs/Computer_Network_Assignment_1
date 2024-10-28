7be823b15b19b78a26c43219c1afe7927ac64224
tienkhang <khangtien24@gmail.com>
2024-10-28 21:40:06 +0700

    mini implementation from the kurose textbook

--- server.py
+++ server.py
@@ -9,4 +9,12 @@ abstraction for easy access later!
 '''
 def identify_conn(port,addr):
     return port, addr
+'''
+I'm pulling this code from our Kurose textbook
+just simulating and experimenting purposes only
+'''
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', server_port))
+print('The server is ready to receive')
 
