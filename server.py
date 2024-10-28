1a6af19b3295eb7faf68ec7da728e861879f90ec
tienkhang <khangtien24@gmail.com>
2024-10-28 21:37:19 +0700

    identifier?? abstraction for ease of info retrieval

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
 from socket import*
 from threading import thread
+
+'''
+identifier includes both IP address
+and port numbers associated with
+process on host.
+abstraction for easy access later!
+'''
+def identify_conn(port,addr):
+    return port, addr
+
