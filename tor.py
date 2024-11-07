a14ec05e11bb23d7e4551a91a1312acf400b935c
khangtien <khangtien24@gmail.com>
2024-11-08 01:50:17 +0700

    why did i even added magnet????

--- tor.py
+++ tor.py
@@ -1,12 +1,24 @@
-import bencodepy
+import hashlib
+import sys
+piece_size = 512000
+
+
 class torrent:
     def __init__(self):
         pass 
-    def __init__(self, a,b):
+    def __init__(self, tracker_id,pcnt,mode): #mode here is either single/multiple file
         pass
+    def set_trackerid(tracker):
+        tracker_id = tracker
+    def setpcnt(cnt):
+        pcnt = cnt
+    def setmode(m):
+        mode = m
+    def getmode(self):
+        return self.mode
+    
+class singlef_tor:
+    
+    
 
 
-
-torr = {
-    "announce": 
-}
