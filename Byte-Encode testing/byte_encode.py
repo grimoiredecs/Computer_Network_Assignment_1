fb936b7fccc85676e16ebc5dc403e6fc297d736e
khangtien <khangtien24@gmail.com>
2024-11-07 16:04:52 +0700

    just ran this (output into raw byte) very interesting results!!!!

--- Byte-Encode testing/byte_encode.py
+++ Byte-Encode testing/byte_encode.py
@@ -1,5 +1,10 @@
 import sys
 
 import re #regex
-
+'''
+ open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+'''
  
+with open('Reference_NetApp.pdf', 'rb') as file: #raw byte reading
+    raw_byte = file.read()  
+    print(raw_byte)
