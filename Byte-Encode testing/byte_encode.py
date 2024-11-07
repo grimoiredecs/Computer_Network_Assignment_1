e7f51d27ee7c1c592b245233e46f8228dae282fb
khangtien <khangtien24@gmail.com>
2024-11-07 18:51:12 +0700

    added example alice.torrent file (courtesy of https://github.com/webtorrent/webtorrent-fixtures)

--- Byte-Encode testing/byte_encode.py
+++ Byte-Encode testing/byte_encode.py
@@ -1,5 +1,6 @@
 import sys
-
+import os
+from bencode import*
 import re #regex
 '''
  open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
@@ -36,7 +37,11 @@ with open('Reference_NetApp.pdf', 'rb') as file: #raw byte reading
 
 '''
 
+byts = b'0x0009'
 
 with open('new.txt','rb') as file:
     raw_txt = file.read()
     print(raw_txt)
+
+
+dec_match = re.compile('\d') #demical (0-9) regex matching
